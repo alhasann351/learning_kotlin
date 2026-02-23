006d7f750b40f0087ba058a54ccba2ae85bc652c
Al-Hasan <alhasann352@gmail.com>
2026-02-24 01:07:31 +0600

    data-type-example-and-notes

---
+++ notes/DataTypes.md
@@ -0,0 +1,7 @@
+INTEGER → Byte, Short, Int, Long
+
+FLOATING POINT → Float, Double
+
+BOOLEAN → True, False
+
+CHARACTER → Char, String
---
+++ src/DataTypes.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name: String = "Hasan"
+    var age: Int = 26
+    var point: Float = 100.0f
+    var largeNumber: Double = 1000000000.0000000000
+    var firstWord: Char = 'H'
+    var isHasan: Boolean = true
+
+    println(name)
+    println(age)
+    println(point)
+    println(largeNumber)
+    println(firstWord)
+    println(isHasan)
+}
