7c1fa7519dfb8256008abfb071e482381226d048
Al-Hasan <alhasann352@gmail.com>
2026-02-27 02:47:36 +0600

    input-output

---
+++ src/InputOutput.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print("Enter your name: ")
+    val name : String? = readLine()
+    print("Your name is: $name")
+    println()
+    print("Enter your age: ")
+    val age : String? = readLine()
+    print("Your age is: $age")
+}
