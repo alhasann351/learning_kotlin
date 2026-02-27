ba79b090eb18eaa57339b3b309e6439256b61f9a
Al-Hasan <alhasann352@gmail.com>
2026-02-27 20:40:47 +0600

    higher-order-function

--- src/Function.kt
+++ src/Function.kt
@@ -10,6 +10,25 @@ fun main() {
     val multi2 : Int = mul(5, 2)
     println("Multiplication: $multi2")
     namedDefault(name = "Hasan", age = 18)
+
+    println("Higher Order Function:")
+    higherOrder(6, 6) { myRes ->
+        println(myRes)
+    }
+
+    higherOrder2(6, myTul = {
+        println(it)
+    }, 4)
+
+    higherOrder3(2, myTul = { myAge, myName ->
+        println("$myAge $myName")
+    } ,2)
+
+    higherOrder4(1, myTul2 = {myR, myN ->
+        println("$myR $myN")
+        myR + 12
+    },2)
+
 }
 
 fun add() {
@@ -33,4 +52,26 @@ fun mul(a: Int, b: Int) : Int {
 
 fun namedDefault(name: String, age: Int = 0) {
     println("Name: $name, Age: $age")
+}
+
+//higher order function lambda example
+fun higherOrder (i: Int, j: Int, myOrder: (Int) -> Unit) {
+    val res = i + j
+    myOrder(res)
+}
+
+fun higherOrder2 (i: Int, myTul: (Int) -> Unit, j: Int) {
+    val res = i + j
+    myTul(res)
+}
+
+fun higherOrder3 (i: Int, myTul: (Int, String) -> Unit, j: Int) {
+    val res = i + j
+    myTul(res, "Hasan")
+}
+
+fun higherOrder4 (i: Int, myTul2: (Int, String) -> Int, j: Int) {
+    val res = i + j
+    val tul = myTul2(res, "Hasan")
+    println(tul)
 }
